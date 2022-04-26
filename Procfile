3a6990c855ece30e52aadd68d6c826a690a18dcd
Pranav Arora <aurorapranav187@gmail.com>
2022-04-26 09:41:37 +0530

    Initial Commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn sportpredictor.wsgi --log-file=-
---
+++ dashboard/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Data
+# Register your models here.
+
+
+class DataAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'height', 'sex', 'predictions')
+
+
+admin.site.register(Data, DataAdmin)
